8effd5bde55738bcb2616917af48a4a263c958f3
everoddandeven <everoddandeven@protonmail.com>
2025-08-28 20:11:19 +0200

    Replace NUnit with xUnit

--- Monero.Test/TestMoneroWalletLight.cs
+++ Monero.Test/TestMoneroWalletLight.cs
@@ -5,15 +5,8 @@ namespace Monero.Test
 {
     public class TestMoneroWalletLight : TestMoneroWalletCommon
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+        public TestMoneroWalletLight() {
+        
         }
 
         protected override void CloseWallet(MoneroWallet wallet, bool save)
