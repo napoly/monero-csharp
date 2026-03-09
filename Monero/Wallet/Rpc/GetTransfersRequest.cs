1a85fc74f18ff7f2cadd3ce20f84198b28bfe2ea
Deverick <5827364+deverickapollo@users.noreply.github.com>
2026-03-09 09:40:34 -0500

    Fix JSON property name for FilterByHeight

--- Monero/Wallet/Rpc/GetTransfersRequest.cs
+++ Monero/Wallet/Rpc/GetTransfersRequest.cs
@@ -9,7 +9,7 @@ public class GetTransfersRequest
     [JsonPropertyName("pending")] public bool Pending { get; set; }
     [JsonPropertyName("failed")] public bool Failed { get; set; }
     [JsonPropertyName("pool")] public bool Pool { get; set; }
-    [JsonPropertyName("filter_by_height ")] public bool? FilterByHeight { get; set; }
+    [JsonPropertyName("filter_by_height")] public bool? FilterByHeight { get; set; }
     [JsonPropertyName("min_height")] public uint? MinHeight { get; set; }
     [JsonPropertyName("max_height")] public uint? MaxHeight { get; set; }
     [JsonPropertyName("account_index")] public long? AccountIndex { get; set; }
